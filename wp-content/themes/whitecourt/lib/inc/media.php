ce421ec1ebb442e0e6da7700344f633c8328373a
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2013-08-20 11:15:08 -0400

    Respond to client comments regarding attorney pages

--- wp-content/themes/whitecourt/lib/inc/media.php
+++ wp-content/themes/whitecourt/lib/inc/media.php
@@ -8,7 +8,7 @@ function add_viewport_meta_tag() {
 
 /** Add new image sizes */
 add_image_size( 'post-image', 540, 150, TRUE );
-add_image_size( 'headshot', 340, 600, FALSE );
+add_image_size( 'headshot', 373, 600, FALSE );
 add_image_size( 'mini-headshot', 120, 120, TRUE );
 
 /* Manipulate the featured image */
