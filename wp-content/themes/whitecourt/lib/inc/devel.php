34f96d5724f4a7b9d5711147cfd19b874a256a00
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2013-11-15 21:33:01 -0500

    tempporary function to match practice areas to practice area pages

--- wp-content/themes/whitecourt/lib/inc/devel.php
+++ wp-content/themes/whitecourt/lib/inc/devel.php
@@ -9,6 +9,19 @@ function get_hooks(){
     
 global $wp_filter; ts_var( $wp_filter['get_the_excerpt'] );
 }
+
+add_action('wp_head','list_all_practice_areas');
+
+function list_all_practice_areas(){
+    global $msd_lawfirm;
+    $practice_areas = $msd_lawfirm->display_class->get_all_practice_areas();
+    foreach($practice_areas AS $pa){
+        $mypage = get_page_by_path('/practice-areas/'.$pa->slug);
+        $has_page = $mypage?'TRUE':'FALSE';
+        $content .= $pa->name.','.$pa->slug.','.$has_page."\n";
+    }
+    ts_data($content);
+}
 /*
 * A useful troubleshooting function. Displays arrays in an easy to follow format in a textarea.
 */
