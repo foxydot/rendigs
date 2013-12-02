0fcdb770b7d88d005332d37c4e313b6d62dc2260
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2013-12-02 13:37:14 -0500

    update wpalchemy

--- wp-content/plugins/msd-lawfirm-cpt/lib/template/newsletter-info.php
+++ wp-content/plugins/msd-lawfirm-cpt/lib/template/newsletter-info.php
@@ -4,8 +4,7 @@
     <li class="gfield even" id="field_newsletter_pdf"><label for="<?php $mb->the_name(); ?>"
         class="gfield_label">Newsletter PDF File</label>
     <div class="ginput_container last-child even">
-        <?php $wpalchemy_media_access->setGroupName('newsletter_pdf'. $mb->get_the_index())->setInsertButtonLabel('Insert This')->setTab('gallery'); ?>
-        
+        <?php $wpalchemy_media_access->setGroupName('newsletter_pdf'. $mb->get_the_index())->setInsertButtonLabel('Insert This')->setTab('gallery'); ?>     
         <?php echo $wpalchemy_media_access->getField(array('name' => $mb->get_the_name(), 'value' => $mb->get_the_value())); ?>
         <?php echo $wpalchemy_media_access->getButton(array('label' => '+')); ?>
         </div>
