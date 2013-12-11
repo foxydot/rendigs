95e8920d8513764eec52829350fce2674f757b25
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2013-12-11 14:34:04 -0500

    Apparently I had started on something interesting with past events and forgot to remove it.

--- wp-content/themes/whitecourt/taxonomy-event_category.php
+++ wp-content/themes/whitecourt/taxonomy-event_category.php
@@ -4,7 +4,7 @@
 remove_action( 'genesis_loop', 'genesis_do_loop' );
 add_action( 'genesis_loop', 'genesis_custom_loop' );
 add_filter('genesis_custom_loop_args','targeted_events_do_custom_loop');
-add_filter('genesis_custom_loop_args','targeted_past_events_do_custom_loop');
+//add_filter('genesis_custom_loop_args','targeted_past_events_do_custom_loop');
 
 remove_all_actions('genesis_post_content');
 
@@ -13,36 +13,41 @@ add_filter( 'genesis_noposts_text', 'msd_noevents_text');
 function targeted_events_do_custom_loop() {
     global $paged; // current paginated page
     global $wp_query;
+    $today = current_time( 'timestamp' ) - 86400;
+    $six_months = mktime(0, 0, 0, date("m",$today)+6, date("d",$today), date("Y",$today));
     $args = array(
         'meta_query' => array(
+            'relation' => 'AND',
             array(
                 'key' => '_date_event_datestamp',
-                'value' => time()-86400,
+                'value' => $today,
                 'compare' => '>'
             ),
             array(
                 'key' => '_date_event_datestamp',
-                'value' => mktime(0, 0, 0, date("m")+6, date("d"), date("Y")),
+                'value' => $six_months,
                 'compare' => '<'
-            )
+            ),
          ),
          'order_by' => 'meta_value',
          'meta_key' => '_date_event_datestamp',
     );
     $test_query = new WP_Query(wp_parse_args($wp_query->query_vars, $args));
     if ( !$test_query->have_posts() ) {
-        add_filter('genesis_custom_loop_args','targeted_past_events_do_custom_loop');
+        //add_filter('genesis_custom_loop_args','targeted_past_events_do_custom_loop');
     }
     return wp_parse_args($wp_query->query_vars, $args);
 }
 function targeted_past_events_do_custom_loop() {
     global $paged; // current paginated page
     global $wp_query;
+    
+    $today = current_time( 'timestamp' ) - 86400;
     $args = array(
         'meta_query' => array(
             array(
                 'key' => '_date_event_datestamp',
-                'value' => time()-86400,
+                'value' => $today,
                 'compare' => '<'
             ),
          ),
